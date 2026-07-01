b47301e79c35736879c3ada5a71b08707499cbb3
Superadmin <Superadmin@01HW1787955>
2026-07-01 18:43:44 +0530

    Div () added

--- src/main/java/gfg/devops/MyCalculator.java
+++ src/main/java/gfg/devops/MyCalculator.java
@@ -9,11 +9,22 @@ public class MyCalculator {
 	{
 		return a-b;
 	}
+	public int mul (int a, int b)
+	{
+		return a*b;
+	}
+	
+	public int div (int a, int b)
+	{
+		return a/b;
+	}
 	
 	public static void main(String[] args) {
 		MyCalculator calc = new MyCalculator();
 		System.out.println ("sum is :"+calc.sum(20, 10));
 		System.out.println ("diff is :"+calc.diff(20, 10));
+		System.out.println ("Mul is :"+calc.mul(20, 10));
+		System.out.println ("Div is :"+calc.div(20, 10));
 	}
 
 }
